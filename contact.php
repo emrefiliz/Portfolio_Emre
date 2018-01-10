43f65397d8d573b5e51bb8ac76d6489e710b40b1
Emre Filiz <e_filiz@fanshawonline.ca>
2018-01-10 17:28:13 -0500

    php revised

--- contact.php
+++ contact.php
@@ -1,36 +1,133 @@
 <?php
-if(isset($_POST['submit'])){
-    $to = "emre@emrefiliz.com";
-    $from = $_POST['email'];
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+	if(isset($_POST['submit'])){
+		$to = "emre@emrefiliz.com";
+		$from = $_POST['email'];
+		$fullname = $_POST['fullname'];
+		$subject = "A new contact for porfolio";
+		$message = "A message from: " . $fullname
+					. "\n\n Message" . $_POST['message']
+					. "\n\n Telephone" . $_POST['telephone'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-  
-    }
+
+		$headers = "From: " . $from;
+
+		mail($to,$subject,$message,$headers);
+
+		redirect_to("result.html");
+
+
+	}
+
+			function redirect_to($location){
+				if($location != NULL){
+					header("Location: {$location}");
+					exit;
+		}
+}
 ?>
 
-<!DOCTYPE html>
+
+<!doctype html>
+<html lang="en">
 <head>
-<title>Form submission</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Emre Filiz</title>
+	<link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="styles/reset.css">
+	<link rel="stylesheet" href="styles/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Days+One" rel="stylesheet">
 </head>
+
 <body>
 
-<form action="" method="post">
-First Name: <input type="text" name="first_name"><br>
-Last Name: <input type="text" name="last_name"><br>
-Email: <input type="text" name="email"><br>
-Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
-<input type="submit" name="submit" value="Submit">
-</form>
+	<h1 class="hidden">Menu</h1>
+	<div id="container">
+		<div class="row">
+			<div class="small-12 large-2 columns">
+				<img data-interchange="[images/emrfiliz_logo.jpg, small], [images/emrfiliz_logo.jpg, large]" alt="Emre Filiz Logo" class="logo">
+			</div>
+
+			<div class="small-12 large-8 large-offset-2 columns">
+				<div class="title-bar" id="toper" data-responsive-toggle="mainmenu" data-hide-for="medium">
+					<button class="menu-icon float-right" type="button" data-toggle="mainmenu"></button>
+				</div>
+
+				<div class="top-bar" id="mainmenu">
+					<div class="top-bar-left">
+						<ul class="dropdown menu" data-dropdown-menu>
+
+							<li>
+								<ul class="dropdown menu" data-dropdown-menu>
+									<li><a href="index.html">Home</a></li>
+									<li><a href="work.html">Works</a></li>
+									<li><a href="contact.php">Contact</a></li>
+								</ul>
+							</li>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="work.html">Works</a></li>
+							<li><a href="contact.php">Contact</a></li>
+						</ul>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+
+		<main>
+
+
+			<div class="row">
+				<div class="columns large-6 medium-6 medium-centered small-12 small-centered large-centered">
+					<div id="ContactWrapper">
+						<form id="ContactArea" method="POST" action="">
+							<h2 class="hidden">Reg Form</h2>
+							<p id="ContactText"> Please send me your message.</p>
+							<label for="name"> Name: </label>
+
+							<input type="text" class="RD" required id="fullname" name="fullname" size="30" placeholder="please enter your name">
+
+							<label for="name"> E-mail address: </label>
+
+							<input type="text" class="RD" required id="email" name="email" size="30" placeholder="please enter your e-mail address">
+
+							<label for="name"> Telephone number </label>
+
+							<input type="text" class="RD" required id="telephone" name="telephone" size="30" placeholder="please enter your telephone#">
+
+							<label for="comments"> Your Message: </label>
+
+							<textarea type="comments" class="RD" id="message" name="message" required cols="100" rows="12"> </textarea>
+
+							<input type="submit" required id="submit" name="submit" value="submit" class="float-right">
+						</form>
+
+					</div>
+				</div>
+			</div>
+
+			<footer>
+				<section>
+					<h2 class="hidden">footer</h2>
+					<img data-interchange="[images/footer_3.jpg, small], [images/footer_1.jpg, large]" alt="Emre Filiz � 2018. All Rights Reserved.">
+				</section>
+
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="work.html">Works</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</footer>
+		</main>
+
+	</div>
+
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+	<script>$(document).foundation();</script>
+	<script src="js/main.js"></script>
 
 </body>
 </html>
